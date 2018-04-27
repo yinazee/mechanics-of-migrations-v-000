91228c97639ca4710f8a7cd6b4781df28d79700d
Yina Zhu <yinagit@gmail.com>
2018-04-27 23:51:30 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,9 @@ require 'date'
 require 'bundler/setup'
 Bundler.require
 
-
-# put the code to connect to the database here
-
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
 require_relative "../artist.rb"
